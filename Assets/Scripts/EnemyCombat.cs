58f8b36525f3db541f4c9917fb8fd9cc3d4fa393
Matt <bendermaj@gmail.com>
2020-10-06 12:17:09 -0400

    EnemyHP + Knockback
    -Enemies have hp and have takeDamage(int dmg)
    -Upon taking damage they get hit back although it currently breaks their pathing

---
+++ Assets/Scripts/EnemyCombat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+    private int hp = 10;
+    public Rigidbody rb;
+    public Transform target;
+    public int knockDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            takeDamage(1);
+        }
+    }
+    private void takeDamage(int dmg)
+    {
+        hp -= dmg;
+        //Knockback
+        rb.AddForce(Vector3.back * knockDistance);
+    }
+}
