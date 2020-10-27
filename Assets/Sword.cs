7b0ae36c8372d6dad959b67f0d98ac6a0e48f205
thuggishPugs <c.walker2672@gmail.com>
2020-10-27 13:57:46 -0400

    updated sword to access player script and copy isAttacking to swords local attacking variable, sword only deals dmg if attacking = true

--- Assets/Sword.cs
+++ Assets/Sword.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    private bool Attacking = false;
+
+    private GameObject Player;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
-            collision.gameObject.SendMessage("TakeDamage", 1);
+            if (Attacking)
+            {
+                collision.gameObject.SendMessage("TakeDamage", 1);
+            }
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindGameObjectWithTag("Player");    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Attacking = Player.GetComponent("isAttacking");
     }
 }
