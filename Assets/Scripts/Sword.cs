69793e42fce51efba250c4a73ec53a6fad605b22
thuggishPugs <c.walker2672@gmail.com>
2020-11-04 14:11:31 -0500

    changed to trigger, added rigidbody

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -8,7 +8,7 @@ public class Sword : MonoBehaviour
 
     private GameObject Player;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
