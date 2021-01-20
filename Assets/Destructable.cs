7cdaf586a69b86235fe979f5be78ceff0e9f3271
varant-t <vv_titizian@hotmail.ca>
2021-01-20 13:12:58 -0500

    Box Breakle Script and Components Added

---
+++ Assets/Destructable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+    private void OnMouseDown()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
