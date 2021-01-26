cb8e32e29dc38c04e51ce295a27b9330e7da6d5a
RossACurry <rossacurry@gmail.com>
2021-01-26 00:21:23 -0500

    Created HammerGame, Ross&Tim Scene, WallObstacle
    -Created the "Test your stength" hammer game. Various objects until we get art assets.
    -Created test scene for Ross & Tim so we can work together on unlocking new areas from minigames. Copied main scene, and renamed to do this. Will be careful not to mess with prefabs, my previous scene was broken so I had to.
    -Created Wall obstacle that the player can only open by winning the hammer game.

---
+++ Assets/Scripts/Enviorment Scripts/HammerGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerGame : MonoBehaviour
+{
+    /*When player uses their hammer smash ability on the Test Your Strength Game, the box will fly up and hit the bell!
+    This will open a new area for the player to explore.
+    Target will be the bell, this script is attached to the moving part of the game at bottom. (Square) For now.*/
+    public GameObject target;
+    public bool isHit = false;
+
+    /*
+     * Put the wall opening from hammer game code below here 
+     */
+    public bool wallOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHit == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 10f * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collision other)
+    {
+        //Check if player has used the hammer smash to play game
+        if (CompareTag("HammerSmash")) {
+            Debug.Log("Hit Hammer Game with hammer smash!");
+                isHit = true;
+        }
+
+        //Check if player has won the game and opened the new area
+        if (other.gameObject.CompareTag("Bell")) {
+            Debug.Log("Player has won Hammer Game!");
+            wallOpen = true;
+        }
+    }
+}
