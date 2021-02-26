b3a25c4283aa55088c5617a74e8a7d184bfb0ce1
varant-t <vv_titizian@hotmail.ca>
2021-02-26 12:01:40 -0500

    Crates now drop random pickups, or nothing

--- Assets/Scripts/Enviorment Scripts/Destructable.cs
+++ Assets/Scripts/Enviorment Scripts/Destructable.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class Destructable : MonoBehaviour
 {
     public GameObject destroyedVersion;
+    public GameObject[] pickUps;
+    public int index;
+   
+
+    private void Start()
+    {
+      
+    }
+
+    private void Update()
+    {
+        index = Random.Range(0, pickUps.Length);
+
+      
+
+    }
     private void OnMouseDown()
     {
-        Instantiate(destroyedVersion, transform.position, transform.rotation);
+
+        GameObject brokenVersion = Instantiate(destroyedVersion, transform.position, transform.rotation, destroyedVersion.transform.parent);
+        brokenVersion.transform.localScale = new Vector3(2, 2, 2);
+        Object.Destroy(brokenVersion, 5f);
+        
+     
+
+
+        GameObject randomPickup = Instantiate(pickUps[index], transform.position, transform.rotation);
+        randomPickup.transform.localScale = new Vector3(1, 1, 1);
+
+       // Instantiate(destroyedVersion, transform.position, transform.rotation, destroyedVersion.transform.parent);
         Destroy(gameObject);
     }
 }
