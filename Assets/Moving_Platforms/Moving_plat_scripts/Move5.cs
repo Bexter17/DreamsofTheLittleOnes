13cf634e81b160ec7d878f1e168603fc0f80ca65
TimMacaraeg <64982604+TimMacaraeg@users.noreply.github.com>
2021-03-10 09:07:35 -0500

    Added 1 moving platform

---
+++ Assets/Moving_Platforms/Moving_plat_scripts/Move5.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move5 : MonoBehaviour
+{
+    bool flag = true;
+
+    [SerializeField]
+    Transform platform;
+
+    [SerializeField]
+    Transform endTransform;
+
+    [SerializeField]
+    float platformSpeed;
+
+    Vector3 direction;
+    Transform destination;
+    
+    void Update()
+    {
+
+        if (GameObject.Find("ball5") == null)
+        {
+
+            Debug.Log("Its destroyed");
+            flag = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (flag == false)
+        {
+            platform.GetComponent<Rigidbody>().MovePosition(platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+
+            SetDestination(endTransform);
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        //draw primitives
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(endTransform.position, platform.localScale);
+    }
+
+    void SetDestination(Transform dest)
+    {
+        destination = dest;
+
+        direction = (destination.position - platform.position);
+    }
+}
---
+++ Assets/Moving_Platforms/Moving_plat_scripts/clickOnDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickOnDestroy : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Only destroy spheres 
+                SphereCollider bc = hit.collider as SphereCollider;
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+    }
+}
