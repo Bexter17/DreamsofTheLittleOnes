d8504f2d24b811c52f3e1a5331f670ba139fc1c3
Ross Curry <rossacurry@gmail.com>
2021-03-24 04:20:41 -0400

    Camera Control Update and Reticle
    -Added two cinemachines to Level_1 that are switched on the brain with a script called CameraController.cs
    -Added reticle when player holds down right click.
    -Player switches between cameras with the right click.
    -Added a system for switching between cameras, unfortunately it requires an upgrade to Unity's new input system which corrupts 13 of our scripts. So that is a no! I found a way for it to work which is a good start. Cameras will need future tweaking.
    -

---
+++ Assets/Scripts/Cinemachine/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Cinemachine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    private bool ThirdPersonCamera = true;
+
+    public Image Aimer;
+
+    [SerializeField]
+    public CinemachineVirtualCamera vCam1; //Third Person Camera
+    [SerializeField]
+    public CinemachineVirtualCamera vCam2; //Free Look Camera
+
+    void Awake()
+    {
+        Aimer.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right Mouse Button Clicked");
+            ThirdPersonCamera = false;
+            Aimer.enabled = true;
+            SwitchPriority();
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            Debug.Log("Right Mouse Button Let go");
+            ThirdPersonCamera = true;
+            Aimer.enabled = false;
+            SwitchPriority();
+        }
+    }
+
+    private void SwitchPriority()
+    {
+        Debug.Log("Switch State Running");
+        if (ThirdPersonCamera)
+        {
+            vCam1.Priority = 3;
+        }
+        else
+        {
+            vCam2.Priority = 1;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Cinemachine/CinemachineStateSwitcher.cs
@@ -0,0 +1,101 @@
+﻿using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*  Cinemachine State Switcher by Ross Curry
+ *  2021 - 03 - 24
+ *  System must use the new unity input system. I commented out all of the new input system and replaced it with functionality for the old input system.
+ */
+public class CinemachineStateSwitcher : MonoBehaviour
+{
+   // [SerializeField]
+   // private InputAction action;
+
+    private Animator animator;
+
+    private bool ThirdPersonCamera = true;
+    private bool FreeLookCamera = false;
+
+    [SerializeField]
+    private CinemachineVirtualCamera vCam1; //Third Person Camera
+    [SerializeField]
+    private CinemachineVirtualCamera vCam2; //Free Look Camera
+
+
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+      //  action.Enable();
+    }
+
+    private void OnDisable()
+    {
+      //  action.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //action.performed += _ => SwitchState();
+        //action.performed += _ => SwitchPriority();
+        //SwitchPriority();
+    }
+
+    private void SwitchState()
+    {
+     //   if(ThirdPersonCamera)
+     //   {
+    //        animator.Play("FreeLookCamera");
+     //   }
+     //   else
+      //  {
+      //      animator.Play("ThirdPersonCamera");
+     //   }
+     //   FreeLookCamera = !FreeLookCamera;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetTrigger("CameraSwitch");
+            Debug.Log("Right Mouse Button Clicked");
+            ThirdPersonCamera = true;
+            FreeLookCamera = false;
+            SwitchPriority();
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            animator.SetTrigger("CameraSwitch");
+            Debug.Log("Right Mouse Button Let go");
+            ThirdPersonCamera = false;
+            FreeLookCamera = true;
+            SwitchPriority();
+        }
+    }
+
+    private void SwitchPriority()
+    {
+        Debug.Log("Switch State Running");
+        if (ThirdPersonCamera)
+        {
+            vCam1.Priority = 1;
+            vCam2.Priority = 0;
+        }
+        else
+        {
+            vCam2.Priority = 1;
+            vCam1.Priority = 0;
+        }
+        FreeLookCamera = !FreeLookCamera;
+        
+    }
+}
