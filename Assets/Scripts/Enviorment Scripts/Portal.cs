10068176bae5733d2110562ccf620d81724a305e
kirixt <s627041041@gmail.com>
2021-03-24 01:28:32 -0400

    update for simple portal
    update for simple portal and portal test scene

---
+++ Assets/Scripts/Enviorment Scripts/Portal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public int targetscene = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (targetscene > 0)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene(targetscene);
+            }
+        }
+    }
+}
