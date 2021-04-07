8d39d91b436cb02903df3eccf5927686f2a07aba
Ross Curry <rossacurry@gmail.com>
2021-04-07 16:39:16 -0400

    Free Aim Ranged Attack
    -Added raycast for free aim ranged attack from cinemachine FreeLook1.
    -Fixed ranged projectile to shoot and follow the raycast.
    -Duplicated glowing orb for debugging.
    -Added AimShoot.cs

---
+++ Assets/Scripts/Player Scripts/AimShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimShoot : MonoBehaviour
+{
+    public float hitForce = 1000f;
+
+    public Camera FreeAim;
+
+    public GameObject rangePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Ray ray = FreeAim.ScreenPointToRay(Input.mousePosition);
+        //100 shoots fairly far, if we wanted to do a sniper like shot double this value
+
+
+        if (Input.GetMouseButton(1) && Input.GetButtonDown("Fire5"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit))   //Shoots directly forward from camera wherever it is looking
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+
+            Vector3 lookdirection = hit.point - transform.position;
+            GameObject bullet = Instantiate(rangePrefab, transform.position, Quaternion.LookRotation(lookdirection)) as GameObject;  //Instantiate projectile and then delete after 5 seconds
+            bullet.GetComponent<Rigidbody>().AddForce(lookdirection * hitForce);
+            Destroy(bullet, 5);
+        }
+    }
+}
+
