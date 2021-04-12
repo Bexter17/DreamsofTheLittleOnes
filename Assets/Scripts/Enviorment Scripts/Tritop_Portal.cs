21696c60f5f8cd0cd089412a7a245dba44dfb51b
varant-t <vv_titizian@hotmail.ca>
2021-04-12 00:57:07 -0400

    Added Tritop Portal / Script, Testing pickup colliders

---
+++ Assets/Scripts/Enviorment Scripts/Tritop_Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tritop_Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("touching");
+            SceneManager.LoadScene("Final_Level");
+        }
+    }
+}
