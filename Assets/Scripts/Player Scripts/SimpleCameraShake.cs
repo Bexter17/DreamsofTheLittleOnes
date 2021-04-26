6a95dd45e392ce1e74fd6e58695ca6ea87733dc1
Ross Curry <rossacurry@gmail.com>
2021-04-26 12:31:28 -0400

    Added Camera Shake Script, HammerSmash Prefab Fix
    -Added SimpleCameraShake.cs
    -Added mesh colliders to other parts of the hammerSmash prefab.

---
+++ Assets/Scripts/Player Scripts/SimpleCameraShake.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+
+public class SimpleCameraShake : MonoBehaviour
+{
+    public float ShakeDuration = 0.3f;
+    public float ShakeAmplitude = 1.2f;
+    public float ShakeFrequency = 2.0f;
+
+    private float ShakeElapsedTime = 0f;
+
+    public bool shakeOn = false;
+    //Cinemachine Shake
+    public CinemachineVirtualCamera VirtualCamera;
+    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get Virtual Camera Noise Profile
+        if (VirtualCamera != null)
+            virtualCameraNoise = VirtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shakeOn)
+        // TESTING, Will switch this to desired ability or attack
+        ShakeElapsedTime = ShakeDuration;
+
+        if(VirtualCamera != null || virtualCameraNoise != null)
+        {
+            // If camera shake effect is still playing
+            if (ShakeElapsedTime > 0)
+            {
+                // Set CInemachine Camera Noise parameters
+                virtualCameraNoise.m_AmplitudeGain = ShakeAmplitude;
+                virtualCameraNoise.m_FrequencyGain = ShakeFrequency;
+
+                // Update Shake Timer
+                ShakeElapsedTime -= Time.deltaTime;
+            }
+            else
+            {
+                // If Camera Shake effect is over, reset variables
+                virtualCameraNoise.m_AmplitudeGain = 0f;
+                ShakeElapsedTime = 0f;
+            }
+        }
+    }
+}
