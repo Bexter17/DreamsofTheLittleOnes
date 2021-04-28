79939a67a9935c962b38e5031ca1b17729fc653a
varant-t <vv_titizian@hotmail.ca>
2021-04-28 14:04:22 -0400

    Tutorial_2 plus UI anchors set

--- Assets/Tutorial_2.cs
+++ Assets/Tutorial_2.cs
@@ -1,33 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using UnityEditor;
 
-public class Tutorial_1 : MonoBehaviour
+public class Tutorial_2 : MonoBehaviour
 {
-    public Transform first_position;
     public GameObject player;
-    public GameObject thing;
     public GameObject tutMenu;
 
-    public bool FreezeTime;
-    public bool done_Tutorial_1;
     // Start is called before the first frame update
     void Start()
     {
-        FreezeTime = false;
         player = GameObject.FindGameObjectWithTag("Player");
-        done_Tutorial_1 = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-     
-       
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,14 +24,14 @@ public class Tutorial_1 : MonoBehaviour
         Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
-           
+
             //Invoke("Kill", 0.1f);
 
-           // tutMenu.SetActive(!tutMenu.activeSelf);
+            // tutMenu.SetActive(!tutMenu.activeSelf);
             tutMenu.SetActive(true);
             if (tutMenu.activeSelf)
             {
-              //pauseAudio.Play();
+                //pauseAudio.Play();
             }
             //if (pauseMenu)
             //{
@@ -55,26 +44,21 @@ public class Tutorial_1 : MonoBehaviour
             //    {
             //        Time.timeScale = 1.0f;
             //    }
-            
+
         }
 
-        
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         tutMenu.SetActive(false);
         Kill();
-        
     }
 
     public void Kill()
-     {
+    {
         Destroy(gameObject);
-     }
-    
+    }
+
 }
-
-
-  
-
