7be3de512d5e02e67643277e3046161c4b816ad2
Cory Rentz <48699510+mrsniffles905@users.noreply.github.com>
2021-05-02 23:53:43 -0600

    Balloon Pop
    Balloon pop and platforms dropping

---
+++ Assets/Scenes/CorysScene/Balloon_Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon_Enemy : MonoBehaviour
+{
+    public ParticleSystem balloonPopParticles;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Balloon"))
+        {
+            Destroy();
+        }
+    }
+
+    public void Destroy()
+    {
+       Instantiate(balloonPopParticles, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
