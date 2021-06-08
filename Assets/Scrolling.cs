5576e39a7a4f1bf01fe2333ae2d201352c606709
Dylan Morrow <75595498+DylanJMorrow@users.noreply.github.com>
2021-06-07 20:46:00 -0500

    Optimize Pass 1 + Credit Scene Update
    Foliage Lods
    Occlusion Culling,
    Collision removal on all objects outside playable area
    Credit Scene update with scrolling and decor

---
+++ Assets/Scrolling.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    public Vector3 speed = new Vector3(0, 0.5f, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime);
+    }
+
+
+
+
+}
